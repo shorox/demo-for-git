63a65a22e1db953c5608c7b1d8a3730edf3f9d2b
Slavik <skazo4nix@gmail.com>
2019-05-20 01:44:57 +0300

    AAA-2 change FirstController (#1)
    * [aaa-1] add FirstController
    
    * [aaa-2] change FirstController

---
+++ src/main/java/com/example/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class FirstController {
+
+  @GetMapping("/test")
+  public ResponseEntity test() {
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @GetMapping("/test2")
+  public ResponseEntity test2() {
+    return new ResponseEntity(HttpStatus.OK);
+  }
+}
