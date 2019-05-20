ec379c117f48d0751a0b9718f17bd426a952d1c6
Slavik <skazo4nix@gmail.com>
2019-05-21 00:24:07 +0300

    add FirstController (#7)

---
+++ src/main/java/com/example/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class FirstController {
+
+  @GetMapping("/test")
+  public ResponseEntity<String> test() {
+    return new ResponseEntity<>("test", HttpStatus.OK);
+  }
+}
